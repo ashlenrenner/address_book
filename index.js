019f22a8bda603948dd628f4ef46bcdbf86927c7
ashlen-renner <ashlenrenner@gmail.com>
2016-03-22 23:11:47 -0400

    added css

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
+app.use(express.static('public')); //enables custom css and js
 app.set('view engine', 'ejs');
 
 app.get('/users', user.list); //list page
